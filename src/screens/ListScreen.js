0609a29c12847fce65396e0b65f2f9301f5d2519
MalikKtK <99009682+MalikKtK@users.noreply.github.com>
2024-01-29 17:45:09 +0100

    Section 3 completed

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View, FlatList } from 'react-native';
 
 const ListScreen = () => {
-    return (
-    <View>
-        <Text>List Screen</Text>
-    </View>
+    const friends = [
+        { name: 'Malik #1', age: 20 },
+        { name: 'Malik #2', age: 45 },
+        { name: 'Malik #3', age: 32 },
+        { name: 'Malik #4', age: 27 },
+        { name: 'Malik #5', age: 53 },
+        { name: 'Malik #6', age: 30 },
+        { name: 'Malik #7', age: 23 },
+        { name: 'Malik #8', age: 24 },
+        { name: 'Malik #9', age: 25 },
+    ];
+
+    return ( 
+    <FlatList
+    showsHorizontalScrollIndicator = {false}
+    ketExtractor={(friend) => friend.name}
+    data={friends} 
+    renderItem={({ item }) => {
+        return (
+        <Text style={styles.textStyle}>
+            {item.name} - Age {item.age
+            }</Text>
+        );
+    }} 
+    />
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    textStyle: {
+        marginVertical: 50
+    }
+});
 
 export default ListScreen;
