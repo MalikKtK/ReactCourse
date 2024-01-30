66f98cdef9fac26a7ad3a90c3474664e97dc759a
MalikKtK <99009682+MalikKtK@users.noreply.github.com>
2024-01-30 09:11:18 +0100

    Section  5-6 done

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,29 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hi There!</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+  <View>
+    <Text style={styles.text}>Hi There!</Text>
+    <Button
+    onPress={() => navigation.navigate('Components')} 
+    title="Go to Components Demo" 
+    />
+    <Button
+    title="Go to List Demo"
+    onPress={() => navigation.navigate('List')}
+    />
+    <Button
+    title="Go to Image Demo"
+    onPress={() => navigation.navigate('Image')}
+    />
+    {/* <TouchableOpacity onPress={() => navigation.navigate('List')}>
+      <Text>Go to List Demo</Text>
+      <Text>Go to List Demo</Text>
+      <Text>Go to List Demo</Text>
+    </TouchableOpacity> */}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
